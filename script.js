689281f3a033e4c7bcab949535643ff10db73a64
Eng-fady <109806807+Eng-fady@users.noreply.github.com>
2024-07-12 00:22:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
+    const formData = new FormData(this);
+    const email = formData.get('email');
+    const name = formData.get('name');
+    const message = formData.get('message');
+
+    window.location.href = `mailto:fadyromany390@gmail.com?subject=Message from ${name}&body=${message} (${email})`;
     alert('Thank you for your message!');
 });
